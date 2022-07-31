dccd606fe620c4ddf6a2ba53494a7942fc0645d5
Soumya <aeie2019@rcciit.org.in>
2022-07-31 10:54:24 +0530

    updated ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { useAddress, useMetamask, useDisconnect, useEditionDrop, useToken, useVote, useNetwork } from "@thirdweb-dev/react";
-import {ChainId} from "@thirdweb-dev/sdk";
+import {
+  useAddress,
+  useMetamask,
+  useDisconnect,
+  useEditionDrop,
+  useToken,
+  useVote,
+  useNetwork,
+} from "@thirdweb-dev/react";
+import { ChainId } from "@thirdweb-dev/sdk";
 import { useEffect, useMemo, useState } from "react";
-import {AddressZero} from "@ethersproject/constants";
+import { AddressZero } from "@ethersproject/constants";
 import { SiHiveBlockchain } from "react-icons/si";
-import {BsArrowRightShort} from "react-icons/bs";
+import { BsArrowRightShort } from "react-icons/bs";
 import { GiAmethyst } from "react-icons/gi";
+import twitterLogo from "./assets/twitter-logo.svg";
 
 const App = () => {
-  const editionDrop = useEditionDrop("0x6dEE70a5a86Ce10AF077ae2e8c5820D7Be61C75f");
+  const editionDrop = useEditionDrop(
+    "0x6dEE70a5a86Ce10AF077ae2e8c5820D7Be61C75f"
+  );
   const token = useToken("0x11060ADb42C4E60244088012f4bD2ae93F909AA5");
   const vote = useVote("0x6D6d2b6832e8BDE7f2cdBc70C6194C90c6388599");
 
@@ -16,13 +27,13 @@ const App = () => {
 
   const [membersTokenAmts, setMembersTokenAmts] = useState([]);
   const [memberAddresses, setMemberAddresses] = useState([]);
-  
+
   const [proposals, setProposals] = useState([]);
   const [isVoting, setIsVoting] = useState(false);
   const [hasVoted, setHasVoted] = useState(false);
 
   const [isRinkeby, setIsRinkeby] = useState(true);
-  
+
   const address = useAddress();
   const network = useNetwork();
   const connectWithMetamask = useMetamask();
@@ -31,291 +42,341 @@ const App = () => {
   // console.log("👋 Public address: ", address);
 
   useEffect(() => {
-    if(!address) return;
+    if (!address) return;
 
     const checkBalance = async () => {
       try {
         const nftBalance = await editionDrop.balanceOf(address, 0);
 
-        if(nftBalance.gt(0)) {
+        if (nftBalance.gt(0)) {
           setHasNFT(true);
           console.log("This person has membership NFT.");
         } else {
           setHasNFT(false);
           console.log("This person doesnot have membership NFT");
         }
-      } catch(e) {
+      } catch (e) {
         setHasNFT(false);
         console.log(e);
       }
-    }
+    };
     checkBalance();
-
   }, [address, editionDrop]);
 
   useEffect(() => {
-    if(!hasNFT) return; 
+    if (!hasNFT) return;
 
     const getAllAddresses = async () => {
-        try {
-          const memberAddresses = await editionDrop.history.getAllClaimerAddresses(0);
-          setMemberAddresses(memberAddresses);
-          console.log("Member addresses", memberAddresses);
-        } catch(e) {
-          console.log("Failed to get all addresses", e);
-        }
-    }
+      try {
+        const memberAddresses =
+          await editionDrop.history.getAllClaimerAddresses(0);
+        setMemberAddresses(memberAddresses);
+        console.log("Member addresses", memberAddresses);
+      } catch (e) {
+        console.log("Failed to get all addresses", e);
+      }
+    };
     getAllAddresses();
-    
-  }, [hasNFT, editionDrop.history])
-  
+  }, [hasNFT, editionDrop.history]);
+
   useEffect(() => {
-    if(!hasNFT) return;
+    if (!hasNFT) return;
 
     const getAllBalances = async () => {
       try {
         const memberBalances = await token.history.getAllHolderBalances();
         setMembersTokenAmts(memberBalances);
         console.log("Amount of tokens", memberBalances);
-      } catch(e) {
+      } catch (e) {
         console.log("Failed to get token balances of members", e);
       }
-    }
+    };
     getAllBalances();
-
-  }, [hasNFT, token.history])
+  }, [hasNFT, token.history]);
 
   useEffect(() => {
-    if(!hasNFT) return;
+    if (!hasNFT) return;
 
     const getAllProposals = async () => {
       try {
         const proposals = await vote.getAll();
         setProposals(proposals);
-      } catch(e) {
+      } catch (e) {
         console.log("Failed to get all proposals", e);
       }
-    }
+    };
     getAllProposals();
-
-  }, [hasNFT, vote])
+  }, [hasNFT, vote]);
 
   useEffect(() => {
-    if(!hasNFT) return;
-    if(!proposals.length) return;
+    if (!hasNFT) return;
+    if (!proposals.length) return;
 
     const checkIfUserVote = async () => {
       try {
         const hasVoted = await vote.hasVoted(proposals[0].proposalId);
         setHasVoted(hasVoted);
-        if(hasVoted) {
+        if (hasVoted) {
           console.log("The user already voted");
         } else {
           console.log("The user didnot vote");
         }
-      } catch(e) {
+      } catch (e) {
         console.log("Failed to check if the user voted", e);
       }
-    }
+    };
     checkIfUserVote();
-
-  }, [hasNFT, proposals, address, vote])
+  }, [hasNFT, proposals, address, vote]);
 
   const membersList = useMemo(() => {
     return memberAddresses.map((address) => {
-      const member = membersTokenAmts?.find(({holder}) => holder === address);
+      const member = membersTokenAmts?.find(({ holder }) => holder === address);
 
       return {
         address,
         tokenAmount: member?.balance.displayValue || "0",
-      }
-    })
-  }, [memberAddresses, membersTokenAmts])
+      };
+    });
+  }, [memberAddresses, membersTokenAmts]);
 
-  const mintNFT = async() => {
+  const mintNFT = async () => {
     try {
       setIsClaiming(true);
       await editionDrop.claim("0", 1);
-      console.log(`Successfully minted! Check it out on Opensea: https:testnets.opensea.io/assets/${editionDrop.getAddress()}/0`);
+      console.log(
+        `Successfully minted! Check it out on Opensea: https:testnets.opensea.io/assets/${editionDrop.getAddress()}/0`
+      );
       setHasNFT(true);
-    } catch(e) {
+    } catch (e) {
       setIsClaiming(false);
       console.log(e);
     } finally {
       setIsClaiming(false);
     }
-  }
+  };
 
   const shortenAddress = (str) => {
     return str.substring(0, 6) + "..." + str.substring(str.length - 4);
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     setIsVoting(true);
 
-    const votes = proposals.map(proposal => {
+    const votes = proposals.map((proposal) => {
       const results = {
         proposalId: proposal.proposalId,
         vote: 2,
-      }
+      };
 
-      proposal.votes.forEach(vote => {
-        const radioEl = document.getElementById(proposal.proposalId + "-" + vote.type);
+      proposal.votes.forEach((vote) => {
+        const radioEl = document.getElementById(
+          proposal.proposalId + "-" + vote.type
+        );
 
-        if(radioEl.checked) {
+        if (radioEl.checked) {
           results.vote = vote.type;
           return;
         }
-      })
+      });
       return results;
-    })
+    });
 
     try {
       const delegation = await token.getDelegationOf(address);
-      
-      if(delegation === AddressZero) {
+
+      if (delegation === AddressZero) {
         await token.delegateTo(address);
       }
 
       try {
         await Promise.all(
-          votes.map(async ({proposalId, vote: _vote}) => {
+          votes.map(async ({ proposalId, vote: _vote }) => {
             const proposal = await vote.get(proposalId);
 
-            if(proposal.state === 1) {
+            if (proposal.state === 1) {
               return vote.vote(proposalId, _vote);
             }
 
             return;
           })
-        )
+        );
 
         try {
           await Promise.all(
-            votes.map(async ({proposalId}) => {
+            votes.map(async ({ proposalId }) => {
               const proposal = await vote.get(proposalId);
 
-              if(proposal.state === 4) {
+              if (proposal.state === 4) {
                 return vote.execute(proposalId);
               }
 
               return;
             })
-          )
+          );
 
           setHasVoted(true);
           console.log("Successfully voted on the proposal.");
-        } catch(e) {
+        } catch (e) {
           console.log("Failed to execute proposal", e);
         }
-      } catch(e) {
+      } catch (e) {
         console.log("Failed to cast vote", e);
       }
-    } catch(e) {
+    } catch (e) {
       console.log("Failed to delegate tokens", e);
     } finally {
       setIsVoting(false);
     }
-  }
+  };
 
-  
-  if(address && (network?.[0].data.chain.id !== ChainId.Rinkeby)) {
+  if (address && network?.[0].data.chain.id !== ChainId.Rinkeby) {
     return (
       <div className="flex flex-col items-center justify-center h-screen ml-345">
-        <h1 className="text-4xl font-semibold">Please change your network to Rinkeby</h1>
-        <h3 className="text-xl font-medium py-5">This dApp only works on Rinkeby chain</h3>
+        <h1 className="text-4xl font-semibold">
+          Please change your network to Rinkeby
+        </h1>
+        <h3 className="text-xl font-medium py-5">
+          This dApp only works on Rinkeby chain
+        </h3>
       </div>
-    )
+    );
   }
 
   const renderNav = () => {
     return (
-    <div className="top-0 absolute w-full flex items-center justify-between py-7 px-32">
-      <div className="flex items-center">
-        <GiAmethyst className="text-2xl mr-3"/>
-        <h2 className="text-lg font-semibold">Breaking Bad DAO</h2>
+      <div className="top-0 absolute w-full flex items-center justify-between py-7 px-28">
+        <div className="flex items-center">
+          <GiAmethyst className="text-2xl mr-3" />
+          <h2 className="text-lg font-semibold">Breaking Bad DAO</h2>
+        </div>
+        <div className="flex items-center">
+          <SiHiveBlockchain className="text-xl mr-2" />
+          <h2 className="mr-4 text-md font-medium">Rinkeby</h2>
+        </div>
       </div>
-      <div className="flex items-center">
-        <SiHiveBlockchain className="text-xl mr-2"/>
-        <h2 className="mr-4 text-md font-medium">Rinkeby</h2>
-      </div>
-    </div>
-    )
-  }
+    );
+  };
 
-  if(!address) {
-    return (  
+  const footer = () => {
+    return (
+      <div className="bottom-0 absolute flex w-full justify-center">
+        <div className="pr-4 flex items-center font-medium hover:-translate-y-1 duration-100 hover:underline">
+          <img
+            alt="twitter-logo"
+            className="w-9 h-9 object-contain"
+            src={twitterLogo}
+          />
+          <a
+            href="https://twitter.com/_buildspace"
+            target="_blank"
+            rel="noreferrer"
+          >
+            built on @_buildspace
+          </a>
+        </div>
+        <div className="flex items-center font-medium hover:-translate-y-1 duration-100 hover:underline">
+          <img
+            alt="twitter-logo"
+            className="w-9 h-9 object-contain"
+            src={twitterLogo}
+          />
+          <a
+            href="https://twitter.com/iamsoumyass"
+            target="_blank"
+            rel="noreferrer"
+          >
+            built by @iamsoumyass
+          </a>
+        </div>
+      </div>
+    );
+  };
+
+  if (!address) {
+    return (
       <div className="flex items-center justify-center h-screen">
         <div>
           {renderNav()}
-          <h1 className="text-7xl font-semibold text-center pl-345">Welcome to BBDAO</h1>
-          <button 
+          <h1 className="text-7xl font-semibold text-center pl-345">
+            Welcome to BBDAO
+          </h1>
+          <button
             onClick={connectWithMetamask}
             className="flex items-center ml-570 mt-9 py-3 pl-10 pr-8 text-lg text-center font-semibold rounded-full border-2 border-smoky-black hover:scale-105 duration-150 hover:bg-smoky-black hover:text-ocean-green"
           >
             Connect Wallet
-            <span><BsArrowRightShort className="ml-2 text-3xl" /></span>
+            <span>
+              <BsArrowRightShort className="ml-2 text-3xl" />
+            </span>
           </button>
+          {footer()}
         </div>
       </div>
-    )
+    );
   }
 
-  if (hasNFT) {
+  if (!hasNFT) {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
-        <div className="top-0 absolute w-full flex items-center justify-between py-1 pl-233">
+        <div className="top-0 absolute w-full flex items-center justify-between py-1 pl-243">
           <div className="flex items-center">
-            <GiAmethyst className="text-2xl mr-3"/>
-            <h2 className="text-lg font-semibold whitespace-nowrap">Breaking Bad DAO</h2>
+            <GiAmethyst className="text-2xl mr-3" />
+            <h2 className="text-lg font-semibold whitespace-nowrap">
+              Breaking Bad DAO
+            </h2>
           </div>
           <div className="flex items-center ml-345">
-            <SiHiveBlockchain className="text-xl ml-96 mr-2"/>
+            <SiHiveBlockchain className="text-xl ml-96 mr-2" />
             <h2 className="mr-4 text-md font-medium">Rinkeby</h2>
-            <button 
-              className="text-md font-medium border-2 border-smoky-black py-2 px-3 rounded-full my-4 whitespace-nowrap hover:bg-smoky-black hover:text-ocean-green duration-100"  
+            <button
+              className="text-md font-medium border-2 border-smoky-black py-2 px-3 rounded-full my-4 whitespace-nowrap hover:bg-smoky-black hover:text-ocean-green duration-100"
               onClick={disconnectMetamask}
             >
               Disconnect Wallet
             </button>
           </div>
         </div>
-        <h1 className="text-6xl font-semibold ml-270 mt-7">
+        <h1 className="text-6xl font-semibold ml-220 mt-14">
           💎 DAO Member Page
         </h1>
-        <p className="text-lg py-4 ml-300 mb-3">Congratulations! You're a member now</p>
-        <div className="flex flex-row ml-290">
+        <p className="text-lg py-4 ml-290 mb-3">
+          Congratulations! You're a member now
+        </p>
+        <div className="flex flex-row ml-253">
           <div className="flex flex-col items-center mr-20 px-8">
             <h2 className="text-xl font-semibold ml-3 mb-2">Member List</h2>
-            <table className="p-4">
-              <thead>
-                <tr>
-                  <th>Address</th>
-                  <th className="whitespace-nowrap pl-8">Token Amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                {membersList.map((member) => {
-                  return (
-                    <tr key={member.address}>
-                      <td>{shortenAddress(member.address)}</td>
-                      <td className="pl-14">{member.tokenAmount}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+            <div className="bg-white px-3 py-3 rounded-md shadow-md">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Address</th>
+                    <th className="whitespace-nowrap pl-8">Token Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {membersList.map((member) => {
+                    return (
+                      <tr key={member.address}>
+                        <td>{shortenAddress(member.address)}</td>
+                        <td className="pl-14">{member.tokenAmount}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
           <div className="flex flex-col items-center w-96">
-            <h2 className="text-xl font-semibold">Active Proposals</h2>
-            <form
-              onSubmit={(e) => handleSubmit(e)}
-            >
+            <h2 className="text-xl font-semibold mb-2">Active Proposals</h2>
+            <form onSubmit={(e) => handleSubmit(e)}>
               {proposals.map((proposal) => (
-                <div key={proposal.proposalId} className="flex flex-col">
-                  <h5 className="font-semibold py-2">{proposal.description}</h5>
+                <div
+                  key={proposal.proposalId}
+                  className="flex flex-col shadow-md px-6 py-3 bg-white rounded-md mb-3"
+                >
+                  <h5 className="font-semibold pb-3">{proposal.description}</h5>
                   <div className="flex flex-row justify-start">
                     {proposal.votes.map(({ type, label }) => (
                       <div className="pr-16 mb-2" key={type}>
@@ -336,7 +397,7 @@ const App = () => {
                 </div>
               ))}
               <div className="flex flex-col items-center">
-                <button 
+                <button
                   className="border-2 border-smoky-black py-2 px-4 rounded-full w-full font-semibold my-3 hover:bg-smoky-black hover:text-ocean-green duration-150"
                   disabled={isVoting || hasVoted}
                   type="submit"
@@ -344,12 +405,13 @@ const App = () => {
                   {isVoting
                     ? "Voting..."
                     : hasVoted
-                      ? "You Already Voted"
-                      : "Submit Votes"}
+                    ? "You Already Voted"
+                    : "Submit Votes"}
                 </button>
                 {!hasVoted && (
                   <small>
-                    This will trigger multiple transactions that  you will need to sign.
+                    This will trigger multiple transactions that you will need
+                    to sign.
                   </small>
                 )}
               </div>
@@ -358,32 +420,34 @@ const App = () => {
         </div>
       </div>
     );
-  };
-  
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="top-0 absolute w-full flex items-center justify-between py-1 pl-80">
         <div className="flex items-center">
-          <GiAmethyst className="text-2xl mr-3"/>
-          <h2 className="text-lg font-semibold whitespace-nowrap">Breaking Bad DAO</h2>
+          <GiAmethyst className="text-2xl mr-3" />
+          <h2 className="text-lg font-semibold whitespace-nowrap">
+            Breaking Bad DAO
+          </h2>
         </div>
         <div className="flex items-center ml-345">
-          <SiHiveBlockchain className="text-xl ml-96 mr-2"/>
+          <SiHiveBlockchain className="text-xl ml-96 mr-2" />
           <h2 className="mr-4 text-md font-medium">Rinkeby</h2>
-          <button 
-            className="text-md font-semibold border-2 border-smoky-black py-2 px-3 rounded-full my-4 whitespace-nowrap hover:bg-smoky-black hover:text-ocean-green duration-100"  
+          <button
+            className="text-md font-semibold border-2 border-smoky-black py-2 px-3 rounded-full my-4 whitespace-nowrap hover:bg-smoky-black hover:text-ocean-green duration-100"
             onClick={disconnectMetamask}
           >
-          Disconnect Wallet
+            Disconnect Wallet
           </button>
         </div>
       </div>
       <h1 className="text-3xl font-semibold pl-460 my-4">
         Mint your NFT to be a member!
       </h1>
-      <button 
+      <button
         className="ml-460 py-4 px-10 border-2 text-xl font-semibold border-smoky-black rounded-full my-3 hover:scale-105 hover:bg-smoky-black hover:text-ocean-green duration-150"
-        onClick={mintNFT} 
+        onClick={mintNFT}
         disabled={isClaiming}
       >
         {isClaiming ? "Minting..." : "Mint your NFT (FREE)"}
